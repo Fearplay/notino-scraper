a3b214a666113bd2a6600ff5b1ede3e62f67f436
Juraj Acsay <juraj.acsay@seznam.cz>
2024-05-25 22:00:19 +0200

    all sites are scraped

--- scrapers/abstract/scraper_tools.py
+++ scrapers/abstract/scraper_tools.py
@@ -4,20 +4,23 @@ from bs4 import BeautifulSoup
 
 class ScraperTools:
     def __init__(self):
-        self.url = 'https://www.notino.co.uk/toothpaste/'
-        self.get_request = self.get_request()
+        self.page_number = 1
+        self.url = f'https://www.notino.co.uk/toothpaste/?f={self.page_number}-1-2-4891-7183'
+        self.get_request_url = self.get_request()
 
     def get_request(self):
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36",
             "Accept-Language": "en-US,en;q=0.9",
             "Connection": "keep-alive",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+            "Cookie": ""
         }
 
-        request = requests.get(self.url, headers=headers, verify=False)
+        request = requests.get(f'https://www.notino.co.uk/toothpaste/?f={self.page_number}-1-2-4891-7183', headers=headers, verify=False)
+        # print(request.status_code) 403
         return request
 
     def get_content_of_page(self):
-        soup_data = BeautifulSoup(self.get_request.content, 'html.parser')
+        soup_data = BeautifulSoup(self.get_request().content, 'html.parser')
         return soup_data
